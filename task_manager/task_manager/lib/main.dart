ff26c7265c73640b37afc024b9bdbcb8c13bff82
camillebarros <camillebarrossouza@gmail.com>
2025-04-19 13:15:46 -0300

    T002: feat - Implementação do Firebase no arquivo main.dart

--- task_manager/task_manager/lib/main.dart
+++ task_manager/task_manager/lib/main.dart
@@ -1,11 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:task_manager/core/routes/routes.dart';
 import 'package:task_manager/core/themes/app.theme.dart';
+import 'package:task_manager/firebase_options.dart';
 import 'core/routes/app_pages.dart';
 
-
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+   await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform, 
+  );
   runApp(const MyApp());
 }
 
